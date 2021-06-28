96d1866bfc671edae1bdfbc5ee1574e9cf3e7e23
Daniel Ye <danielyeqt@gmail.com>
2021-06-28 14:35:39 -0400

    get keyPressed

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,9 @@ import processing.core.PApplet;
 public class Main extends PApplet {
 
     public static Main client = null;
+    private static boolean[] charPressed = new boolean[500];
+    private static boolean[] codePressed = new boolean[500];
+
     public static final int DEFAULT_WIDTH = 1000;
     public static final int DEFAULT_HEIGHT = 800;
     public static final int DEFAULT_FRAME_RATE = 30;
@@ -24,11 +27,47 @@ public class Main extends PApplet {
         rect(100, 100, 100, 100);
     }
 
+    public void keyPressed(){
+        if(key < charPressed.length){
+            charPressed[key] = true;
+        }
+        if(keyCode < codePressed.length){
+            codePressed[key] = true;
+        }
+    }
+
+    public void keyReleased(){
+        if(key < charPressed.length){
+            charPressed[key] = false;
+        }
+        if(keyCode < codePressed.length){
+            codePressed[key] = false;
+        }
+    }
+
     public void settings() {
         size(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
 
     public static void main(String[] args) {
         PApplet.main(new String[]{"main.Main"});
+    }
+
+    /**
+     * returns if the char k is pressed
+     * @param k the character
+     * @return if k is pressed
+     */
+    public boolean pressed(char k){
+        return charPressed[k];
+    }
+
+    /**
+     * returns if the keyCode is pressed
+     * @param code the key code
+     * @return if code is pressed
+     */
+    public boolean pressed(int code){
+        return codePressed[code];
     }
 }
