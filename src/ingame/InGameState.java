bf338a686f6dce7cec69c8d70d1bde650fa17adb
Daniel Ye <danielyeqt@gmail.com>
2021-06-28 14:49:49 -0400

    Add Ingame and Loading

---
+++ src/ingame/InGameState.java
@@ -0,0 +1,44 @@
+package ingame;
+
+import main.State;
+
+public class InGameState implements State {
+    public InGameState(){
+        setup();
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void keyPressed() {
+
+    }
+
+    @Override
+    public void keyReleased() {
+
+    }
+
+    @Override
+    public void mousePressed() {
+
+    }
+
+    @Override
+    public void mouseReleased() {
+
+    }
+}
---
+++ src/loading/LoadingState.java
@@ -0,0 +1,44 @@
+package loading;
+
+import main.State;
+
+public class LoadingState implements State {
+    LoadingState(){
+        setup();
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void keyPressed() {
+
+    }
+
+    @Override
+    public void keyReleased() {
+
+    }
+
+    @Override
+    public void mousePressed() {
+
+    }
+
+    @Override
+    public void mouseReleased() {
+
+    }
+}
