f3ffcc036b77de23795fb14fb4a7fe8531f18ae8
Daniel Ye <danielyeqt@gmail.com>
2021-06-28 14:59:47 -0400

    load in red Bloon

--- src/main/Assets.java
+++ src/main/Assets.java
@@ -1,10 +1,16 @@
 package main;
 
+import processing.core.PImage;
+
+import static main.Main.client;
+
 /**
  * class holding all of the files we would ever need
  */
 public class Assets {
-    public static void load(){
+    public static PImage redBloon;
 
+    public static void load() {
+        redBloon = client.loadImage("sprites/bloon/redBloon.png");
     }
 }
