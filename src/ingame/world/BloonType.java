4c4852ba59afd053bb95eb3f290fb28be8237837
mars-flat <shanec2k05@gmail.com>
2021-06-29 10:51:56 -0400

    trying to find a decent design pattern

---
+++ src/ingame/world/BloonType.java
@@ -0,0 +1,18 @@
+package ingame.world;
+
+public enum BloonType {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    PINK,
+    BLACK,
+    WHITE,
+    LEAD,
+    ZEBRA,
+    RAINBOW,
+    CERAMIC,
+    MOAB,
+    BFB,
+    ZOMG
+}
--- src/ingame/world/bloons/Bloon.java
+++ src/ingame/world/bloons/Bloon.java
@@ -1,14 +1,32 @@
 package ingame.world.bloons;
 
+import ingame.world.BloonType;
+import ingame.world.DamageType;
+
 // TODO use better design patterns once game is finished
 public class Bloon {
-    private int curBloon;
 
-    Bloon(){
+    public static final int REGEN_COOLDOWN = 60;
+
+    private BloonType curBloon;
+    private BloonType maxBloon;
+    private int health;
+    private int regenTimer;
+    private boolean regen;
+    private boolean camo;
+    private boolean dartResistant;
+    private boolean explosionResistant;
+    private boolean freezeResistant;
+
+
+    public Bloon() {
 
     }
 
-    public void damage(int damage){
+    public void damage(int damage, DamageType damageType) {
+        health -= damage;
+        if (health <= 0) {
 
+        }
     }
 }
