d89c233d885bb984457d9cc435f2ac4ce3f99763
Daniel Ye <danielyeqt@gmail.com>
2021-06-29 11:48:09 -0400

    Add Entity types to world

--- src/ingame/world/World.java
+++ src/ingame/world/World.java
@@ -1,9 +1,19 @@
 package ingame.world;
 
 import ingame.world.bloons.Bloon;
+import ingame.world.towers.Tower;
+import processing.core.PVector;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class World {
     public static World curWorld;
+    private HashSet<Bloon> bloons;
+    private ArrayList<Tower> towers;
+    ArrayList<PVector> path;
+    ArrayList<Entity> vanity;
+    ArrayList<Entity> water;
 
     public void remove(Bloon toRemove){
 
