9d92719ce0abedaec136fbd6ef6aec1acd2a0e26
Daniel Ye <danielyeqt@gmail.com>
2021-06-30 16:37:01 -0400

    Add World Building

---
+++ src/worldBuilder/Circle.java
@@ -0,0 +1,12 @@
+package worldBuilder;
+
+import processing.core.PVector;
+
+public class Circle {
+    PVector center;
+    float radius;
+    Circle(PVector center, float radius){
+        this.center = center;
+        this.radius = radius;
+    }
+}
